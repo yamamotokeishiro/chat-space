ae1ce0cd1058701cb4b9ffd57b9f3015fe02574a
山本圭志朗 <keishiro.ya@gmail.com>
2017-07-17 19:23:44 +0900

    itiou

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
 
   def new
-
+    @group = Group.find(1)
   end
 
   def create
@@ -9,14 +9,20 @@ class GroupsController < ApplicationController
   end
 
   def show
-
+    @group = Group.find(params[:id])
   end
 
   def edit
-
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    @group.update(update_params)
+  end
 
+  private
+  def update_params
+    params.require(:group).permit(:key, :detail)
   end
 end
