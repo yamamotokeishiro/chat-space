eb02a65cfcd1fda983a489ebb108982f1aaa5863
山本圭志朗 <keishiro.ya@gmail.com>
2017-07-20 11:38:29 +0900

    Partial template

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-  has_many : users, through: :GroupsUsers
-  has_many : messages
-  has_many : groupsusers
+  has_many :users, through: :groupsusers
+  has_many :messages
+  has_many :groupsusers
 end
