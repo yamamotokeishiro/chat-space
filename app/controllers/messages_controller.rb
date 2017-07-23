2b9618fd7dc484b1375005cbb39fd5f62d06de1c
山本圭志朗 <keishiro.ya@gmail.com>
2017-07-23 21:32:40 +0900

    message Send

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,11 +3,23 @@ class MessagesController < ApplicationController
   def new
     @group = Group.find(params[:group_id])
     @groups = current_user.groups
-    @message = Message.find(1)
+    @message = Message.new
   end
-end
 
-private
-def message_params
-  params.require(:message).permit(:group_id)
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to new_group_message_path, notice: 'メッセージの作成が完了しました。'
+    else
+      flash.now[:alert] = "メッセージの送信に失敗しました。"
+      @groups = current_user.groups
+      render :new
+    end
+  end
+
+
+  private
+  def message_params
+    params.require(:message).permit(:text, :image).merge(group_id: params[:group_id], user_id: current_user.id)
+  end
 end
