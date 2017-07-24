6736e18f4dd4b6b20408de50a3e59e52c326ccb1
山本圭志朗 <keishiro.ya@gmail.com>
2017-07-24 18:15:25 +0900

    message display

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,7 @@ class MessagesController < ApplicationController
     @group = Group.find(params[:group_id])
     @groups = current_user.groups
     @message = Message.new
+    @messages = Message.where(group_id: params[:group_id])
   end
 
   def create
